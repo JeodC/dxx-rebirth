e6b70df567dd5c10d40a14b2c56d5727e88b1785
Kp <3164941+vLKp@users.noreply.github.com>
2026-01-04 18:42:39 +0000

    Convert `game_mode_flags` to generic enum bit operations

--- common/main/game.h
+++ common/main/game.h
@@ -68,21 +68,27 @@ enum class game_mode_flags : uint16_t
 	/* endif */
 };
 
-static constexpr auto operator&(const game_mode_flags game_mode, const game_mode_flag f)
-{
 #if DXX_USE_MULTIPLAYER
-	return underlying_value(game_mode) & underlying_value(f);
+
+template <>
+inline constexpr bool enable_bit_enum_and<game_mode_flags, game_mode_flag>{true};
+
+template <>
+inline constexpr bool enable_bit_enum_boolnot<game_mode_flags>{true};
+
 #else
+
+constexpr auto operator&(game_mode_flags, game_mode_flag)
+{
 	/* `normal` has the value 0, and every flag with a non-zero value is
 	 * related to multiplayer.  A build without the ability to use multiplayer
 	 * will therefore only ever have a `game_mode` of 0, so the test can be
 	 * simplified to always return false.
 	 */
-	(void)game_mode;
-	(void)f;
 	return std::false_type{};
-#endif
 }
+
+#endif
 
 /* This must be a signed type.  Some sites, such as `bump_this_object`,
  * use Difficulty_level_type in arithmetic expressions, and those
