JeodC/dxx-rebirth

commit ab62cf73dcc69f70e3c3fcca1ef9f4d6c6a6ce03

Use enum class to index polymodel::submodel_rads

similar/main/fireball.cpp
@@ -625,7 +625,7 @@ void explode_badass_player(const d_robot_info_array &Robot_info, const vmobjptri
 
 namespace {
 
-static void object_create_debris(fvmsegptridx &vmsegptridx, const object_base &parent, const int subobj_num, const fix subobject_radius)
+static void object_create_debris(fvmsegptridx &vmsegptridx, const object_base &parent, const submodel_index subobj_num, const fix subobject_radius)
 {
 	Assert(parent.type == OBJ_ROBOT || parent.type == OBJ_PLAYER);
 
@@ -634,12 +634,10 @@ static void object_create_debris(fvmsegptridx &vmsegptridx, const object_base &p
 	if ( obj == object_none )
 		return;				// Not enough debris slots!
 
-	Assert(subobj_num < 32);
-
 	//Set polygon-object-specific data
 
 	obj->rtype.pobj_info.model_num = parent.rtype.pobj_info.model_num;
-	obj->rtype.pobj_info.subobj_flags = 1<<subobj_num;
+	obj->rtype.pobj_info.subobj_flags = 1 << underlying_value(subobj_num);
 	obj->rtype.pobj_info.tmap_override = parent.rtype.pobj_info.tmap_override;
 
 	//Set physics data for this object
@@ -1371,11 +1369,19 @@ static void explode_model(object_base &obj)
 	auto &pm{LevelSharedPolygonModelState.Polygon_models[model_num]};
 	const auto n_models{pm.n_models};
 	if (n_models > 1) {
-		for (unsigned i{1}; i < n_models; ++i)
+		/* Limit the range to the first `n_models`, since later elements are
+		 * not valid for this model.
+		 *
+		 * Skip over the first element, because Descent has always done that.
+		 */
+		for (const auto &&[i, submodel_rad] : enumerate(std::span(pm.submodel_rads).first(n_models).template subspan<1>(), submodel_index{})
+		)
+		{
 #if defined(DXX_BUILD_DESCENT_II)
-			if (!(i == 5 && obj.type == OBJ_ROBOT && get_robot_id(obj) == robot_id::energy_bandit))	//energy sucker energy part
+			if (!(i == submodel_index{5} && obj.type == OBJ_ROBOT && get_robot_id(obj) == robot_id::energy_bandit))	//energy sucker energy part
 #endif
-				object_create_debris(vmsegptridx, obj, i, pm.submodel_rads[i]);
+				object_create_debris(vmsegptridx, obj, i, submodel_rad);
+		}
 
 		//make parent object only draw center part
 		obj.rtype.pobj_info.subobj_flags = 1;

common/include/serial.h
@@ -8,18 +8,17 @@
 #include <algorithm>
 #include <bit>
 #include <cassert>
-#include <cstddef>
 #include <cstdint>
 #include <functional>
 #include <initializer_list>
 #include <tuple>
-#include <type_traits>
 
 #include "dxxsconf.h"
 #include "compiler-range_for.h"
 #include <array>
 #include <memory>
 #include <utility>
+#include "fwd-d_array.h"
 
 namespace serial {
 
@@ -68,6 +67,11 @@ public:
 
 template <typename T>
 class is_cxx_array<const T> : public is_cxx_array<T>
+{
+};
+
+template <typename T, std::size_t N, typename E>
+class is_cxx_array<enumerated_array<T, N, E>> : public is_cxx_array<std::array<T, N>>
 {
 };
 

common/main/polyobj.h
@@ -66,7 +66,12 @@ extern enumerated_array<polygon_model_index, MAX_POLYGON_MODELS, polygon_model_i
 #endif
 
 namespace dcx {
-constexpr std::integral_constant<unsigned, 10> MAX_SUBMODELS{};
+
+constexpr std::size_t MAX_SUBMODELS{10};
+enum class submodel_index : uint8_t
+{
+	/* Valid values are [0 .. (MAX_SUBMODELS - 1)]. */
+};
 
 //used to describe a polygon model
 struct polymodel : prohibit_void_ptr<polymodel>
@@ -78,7 +83,7 @@ struct polymodel : prohibit_void_ptr<polymodel>
 	std::array<vms_vector, MAX_SUBMODELS> submodel_offsets;
 	std::array<vms_vector, MAX_SUBMODELS> submodel_norms;   // norm for sep plane
 	std::array<vms_vector, MAX_SUBMODELS> submodel_pnts;    // point on sep plane
-	std::array<fix, MAX_SUBMODELS> submodel_rads;       // radius for each submodel
+	enumerated_array<fix, MAX_SUBMODELS, submodel_index> submodel_rads;       // radius for each submodel
 	std::array<ubyte, MAX_SUBMODELS> submodel_parents;    // what is parent for each submodel
 	std::array<vms_vector, MAX_SUBMODELS> submodel_mins;
 	std::array<vms_vector, MAX_SUBMODELS> submodel_maxs;

similar/main/polyobj.cpp
@@ -193,11 +193,17 @@ static void read_model_file(polymodel &pm, const char *const filename, robot_inf
 			}
 			
 			case ID_SOBJ: {		//Subobject header
-				int n;
-
-				n = pof_read_short(model_buf, Pof_addr);
-
-				Assert(n < MAX_SUBMODELS);
+				const auto n{pof_read_short(model_buf, Pof_addr)};
+				const auto on{pm.submodel_rads.valid_index(n)};
+				if (!on)
+					/* This leaves the position pointer in an incorrect
+					 * location, but it only happens on ill-formed input.
+					 * Older versions of the game would have undefined behavior
+					 * on this input, so defining the result to be an incorrect
+					 * position is fine.
+					 */
+					break;
+				const submodel_index sn{*on};
 
 				pm.submodel_parents[n] = pof_read_short(model_buf, Pof_addr);
 
@@ -205,7 +211,7 @@ static void read_model_file(polymodel &pm, const char *const filename, robot_inf
 				pof_read_vec(pm.submodel_pnts[n], model_buf, Pof_addr);
 				pof_read_vec(pm.submodel_offsets[n], model_buf, Pof_addr);
 
-				pm.submodel_rads[n] = pof_read_int(model_buf, Pof_addr);		//radius
+				pm.submodel_rads[sn] = pof_read_int(model_buf, Pof_addr);		//radius
 				pm.submodel_ptrs[n] = pof_read_int(model_buf, Pof_addr);	//offset
 
 				break;

commit 1a55853f0e83b527f0a705eb3c1148248176a8d7

Fix robot debris creation
Commit ab62cf73dcc6 introduced an off-by-one error in the value of `i`.
`i` should start at 1 and count upward, but after that commit, it
started at 0, causing the `subobj_flags` of the created debris objects
to be incorrect.  This caused the wrong model to be drawn for the
debris.

Reported-by: bangstk <https://github.com/dxx-rebirth/dxx-rebirth/issues/832>
Analyzed-by: bangstk <https://github.com/dxx-rebirth/dxx-rebirth/issues/832>

Fixes: ab62cf73dcc69f70e3c3fcca1ef9f4d6c6a6ce03 ("Use enum class to index polymodel::submodel_rads")

similar/main/fireball.cpp
@@ -1374,7 +1374,7 @@ static void explode_model(object_base &obj)
 		 *
 		 * Skip over the first element, because Descent has always done that.
 		 */
-		for (const auto &&[i, submodel_rad] : enumerate(std::span(pm.submodel_rads).first(n_models).template subspan<1>(), submodel_index{})
+		for (const auto &&[i, submodel_rad] : enumerate(std::span(pm.submodel_rads).first(n_models).template subspan<1>(), submodel_index{1})
 		)
 		{
 #if DXX_BUILD_DESCENT == 2