JeodC/dxx-rebirth

commit 7bc0594e2147c17d30bc679d98ec6fcecb9830e3

`eclip`: change `changing_object_texture` into union of d1x, d2x
`eclip` is common to both games, except for the member
`changing_object_texture`.  This member is present in both games, but
has a game-specific type since it is an index into the game-specific
`ObjBitmapPtrs`.  Move `enum class object_bitmap_index` to be defined
separately in both `namespace d1x` and `namespace d2x`.  Change `eclip`
to hold a union of these two members, and change callers to access it
through the `dsx` alias, which will resolve to the correct member for
the active game.

This produces the same code as the prior commit.

common/main/bm.h
@@ -70,6 +70,24 @@ inline constexpr bool enable_bit_enum_boolnot<tmapinfo_flags>{true};
 
 }
 
+namespace d1x {
+
+enum class object_bitmap_index : uint16_t
+{
+	None = UINT16_MAX
+};
+
+}
+
+namespace d2x {
+
+enum class object_bitmap_index : uint16_t
+{
+	None = UINT16_MAX
+};
+
+}
+
 #ifdef DXX_BUILD_DESCENT
 namespace dsx {
 #if DXX_BUILD_DESCENT == 1
@@ -168,10 +186,6 @@ constexpr std::integral_constant<unsigned, 610> MAX_OBJ_BITMAPS{};
 extern unsigned N_ObjBitmaps;
 #endif
 
-enum class object_bitmap_index : uint16_t
-{
-	None = UINT16_MAX
-};
 extern enumerated_array<bitmap_index, MAX_OBJ_BITMAPS, object_bitmap_index> ObjBitmaps;
 extern std::array<object_bitmap_index, MAX_OBJ_BITMAPS> ObjBitmapPtrs;
 

common/main/effects.h
@@ -61,7 +61,10 @@ struct eclip : public prohibit_void_ptr<eclip>
 	fix     time_left;      //for sequencing
 	uint32_t frame_count;    //for sequencing
 	texture_index changing_wall_texture;      //Which element of Textures array to replace.
-	object_bitmap_index changing_object_texture;    //Which element of ObjBitmapPtrs array to replace.
+	union {
+		::d1x::object_bitmap_index d1x;    //Which element of d1x::ObjBitmapPtrs array to replace.
+		::d2x::object_bitmap_index d2x;    //Which element of d2x::ObjBitmapPtrs array to replace.
+	} changing_object_texture;
 	int     flags;          //see above
 	int     crit_clip;      //use this clip instead of above one when mine critical
 	texture_index dest_bm_num;    //use this bitmap when monitor destroyed

similar/arch/ogl/ogl.cpp
@@ -433,7 +433,7 @@ void ogl_cache_level_textures(void)
 	range_for (auto &ec, partial_const_range(Effects, Num_effects))
 	{
 		ogl_cache_vclipn_textures(Vclip, ec.dest_vclip);
-		if (ec.changing_wall_texture == texture_index{UINT16_MAX} && ec.changing_object_texture == object_bitmap_index::None)
+		if (ec.changing_wall_texture == texture_index{UINT16_MAX} && ec.changing_object_texture.dsx == object_bitmap_index::None)
 			continue;
 		if (ec.vc.num_frames>max_efx)
 			max_efx=ec.vc.num_frames;
@@ -442,7 +442,7 @@ void ogl_cache_level_textures(void)
 	for (ef=0;ef<max_efx;ef++){
 		range_for (eclip &ec, partial_range(Effects, Num_effects))
 		{
-			if (ec.changing_wall_texture == texture_index{UINT16_MAX} && ec.changing_object_texture == object_bitmap_index::None)
+			if (ec.changing_wall_texture == texture_index{UINT16_MAX} && ec.changing_object_texture.dsx == object_bitmap_index::None)
 				continue;
 			ec.time_left=-1;
 		}

similar/main/bmread.cpp
@@ -555,7 +555,7 @@ int gamedata_read_tbl(d_level_shared_robot_info_state &LevelSharedRobotInfoState
 	{
 		//Effects[i].bm_ptr = (grs_bitmap **) -1;
 		ec.changing_wall_texture = texture_index{UINT16_MAX};
-		ec.changing_object_texture = object_bitmap_index::None;
+		ec.changing_object_texture.dsx = object_bitmap_index::None;
 		ec.segnum = segment_none;
 		ec.vc.num_frames = -1;		//another mark of being unused
 	}
@@ -839,7 +839,7 @@ int gamedata_read_tbl(d_level_shared_robot_info_state &LevelSharedRobotInfoState
 	//check for refereced but unused clip count
 	for (auto &&[idx, e] : enumerate(Effects))
 	{
-		if ((e.changing_wall_texture != texture_index{UINT16_MAX} || e.changing_object_texture != object_bitmap_index::None) && e.vc.num_frames == ~0u)
+		if ((e.changing_wall_texture != texture_index{UINT16_MAX} || e.changing_object_texture.dsx != object_bitmap_index::None) && e.vc.num_frames == ~0u)
 			Error("EClip %" DXX_PRI_size_type " referenced (by polygon object?), but not defined", idx);
 	}
 
@@ -885,7 +885,7 @@ void verify_textures()
 
 #if DXX_BUILD_DESCENT == 2
 	for (uint_fast32_t i = 0; i < Num_effects; ++i)
-		if (const auto changing_object_texture = Effects[i].changing_object_texture; changing_object_texture != object_bitmap_index::None)
+		if (const auto changing_object_texture{Effects[i].changing_object_texture.dsx}; changing_object_texture != object_bitmap_index::None)
 		{
 			const auto o = ObjBitmaps[changing_object_texture];
 			auto &gbo = GameBitmaps[o];
@@ -1023,14 +1023,14 @@ static std::size_t bm_read_eclip(std::size_t texture_count, int skip)
 		}
 
 		if (obj_eclip) {
-
-			if (Effects[clip_num].changing_object_texture == object_bitmap_index::None)
+			auto &changing_object_texture{Effects[clip_num].changing_object_texture.dsx};
+			if (changing_object_texture == object_bitmap_index::None)
 			{		//first time referenced
-				Effects[clip_num].changing_object_texture = static_cast<object_bitmap_index>(N_ObjBitmaps);		// XChange ObjectBitmaps
+				changing_object_texture = static_cast<object_bitmap_index>(N_ObjBitmaps);		// XChange ObjectBitmaps
 				N_ObjBitmaps++;
 			}
 
-			ObjBitmaps[Effects[clip_num].changing_object_texture] = Effects[clip_num].vc.frames[0];
+			ObjBitmaps[changing_object_texture] = Effects[clip_num].vc.frames[0];
 		}
 
 		//if for an object, Effects_bm_ptrs set in object load
@@ -1445,7 +1445,7 @@ static grs_bitmap *load_polymodel_bitmap(int skip, const char *name)
 	if (name[0] == '%') {		//an animating bitmap!
 		const unsigned eclip_num = atoi(name+1);
 
-		auto &changing_object_texture = Effects[eclip_num].changing_object_texture;
+		auto &changing_object_texture{Effects[eclip_num].changing_object_texture.dsx};
 		// On first reference, changing_object_texture will be None.
 		// Assign it a value.
 		if (changing_object_texture == object_bitmap_index::None)

similar/main/effects.cpp
@@ -67,7 +67,7 @@ void reset_special_effects()
 		if (ec.changing_wall_texture < Textures.size())
 			Textures[ec.changing_wall_texture] = ec.vc.frames[ec.frame_count];
 
-		if (const auto changing_object_texture = ec.changing_object_texture; changing_object_texture != object_bitmap_index::None)
+		if (const auto changing_object_texture{ec.changing_object_texture.dsx}; changing_object_texture != object_bitmap_index::None)
 			ObjBitmaps[changing_object_texture] = ec.vc.frames[ec.frame_count];
 
 	}
@@ -82,7 +82,7 @@ void do_special_effects()
 		const auto vc_frame_time = ec.vc.frame_time;
 		if (!vc_frame_time)
 			continue;
-		if (ec.changing_wall_texture >= Textures.size() && ec.changing_object_texture == object_bitmap_index::None)
+		if (ec.changing_wall_texture >= Textures.size() && ec.changing_object_texture.dsx == object_bitmap_index::None)
 			continue;
 
 		if (ec.flags & EF_STOPPED)
@@ -120,7 +120,7 @@ void do_special_effects()
 			if (ec.changing_wall_texture < Textures.size())
 				Textures[ec.changing_wall_texture] = Effects[n].vc.frames[Effects[n].frame_count];
 
-			if (const auto changing_object_texture = ec.changing_object_texture; changing_object_texture != object_bitmap_index::None)
+			if (const auto changing_object_texture{ec.changing_object_texture.dsx}; changing_object_texture != object_bitmap_index::None)
 				ObjBitmaps[changing_object_texture] = Effects[n].vc.frames[Effects[n].frame_count];
 
 		}
@@ -128,7 +128,7 @@ void do_special_effects()
 			if (ec.changing_wall_texture < Textures.size())
 				Textures[ec.changing_wall_texture] = ec.vc.frames[ec.frame_count];
 	
-			if (const auto changing_object_texture = ec.changing_object_texture; changing_object_texture != object_bitmap_index::None)
+			if (const auto changing_object_texture{ec.changing_object_texture.dsx}; changing_object_texture != object_bitmap_index::None)
 				ObjBitmaps[changing_object_texture] = ec.vc.frames[ec.frame_count];
 		}
 
@@ -144,7 +144,7 @@ void restore_effect_bitmap_icons()
 			if (ec.changing_wall_texture < Textures.size())
 				Textures[ec.changing_wall_texture] = ec.vc.frames[0];
 	
-			if (const auto changing_object_texture = ec.changing_object_texture; changing_object_texture != object_bitmap_index::None)
+			if (const auto changing_object_texture{ec.changing_object_texture.dsx}; changing_object_texture != object_bitmap_index::None)
 				ObjBitmaps[changing_object_texture] = ec.vc.frames[0];
 		}
 	}
@@ -166,7 +166,7 @@ void stop_effect(int effect_num)
 	if (ec->changing_wall_texture < Textures.size())
 		Textures[ec->changing_wall_texture] = ec->vc.frames[0];
 	
-	if (const auto changing_object_texture = ec->changing_object_texture; changing_object_texture != object_bitmap_index::None)
+	if (const auto changing_object_texture{ec->changing_object_texture.dsx}; changing_object_texture != object_bitmap_index::None)
 		ObjBitmaps[changing_object_texture] = ec->vc.frames[0];
 }
 
@@ -180,7 +180,7 @@ void restart_effect(int effect_num)
 }
 
 DEFINE_VCLIP_SERIAL_UDT();
-DEFINE_SERIAL_UDT_TO_MESSAGE(eclip, ec, (ec.vc, ec.time_left, ec.frame_count, ec.changing_wall_texture, ec.changing_object_texture, ec.flags, ec.crit_clip, ec.dest_bm_num, serial::pad<2>(), ec.dest_vclip, serial::pad<3>(), ec.dest_eclip, ec.dest_size, ec.sound_num, serial::pad<3>(), ec.segnum, serial::pad<2>(), ec.sidenum, serial::pad<3>()));
+DEFINE_SERIAL_UDT_TO_MESSAGE(eclip, ec, (ec.vc, ec.time_left, ec.frame_count, ec.changing_wall_texture, ec.changing_object_texture.dsx, ec.flags, ec.crit_clip, ec.dest_bm_num, serial::pad<2>(), ec.dest_vclip, serial::pad<3>(), ec.dest_eclip, ec.dest_size, ec.sound_num, serial::pad<3>(), ec.segnum, serial::pad<2>(), ec.sidenum, serial::pad<3>()));
 ASSERT_SERIAL_UDT_MESSAGE_SIZE(eclip, 130);
 
 /*

similar/main/paging.cpp
@@ -133,7 +133,7 @@ static void paging_touch_object_effects(const d_eclip_array &Effects, const obje
 {
 	range_for (auto &i, partial_const_range(Effects, Num_effects))
 	{
-		if ( i.changing_object_texture == tmap_num )	{
+		if (i.changing_object_texture.dsx == tmap_num) {
 			paging_touch_vclip(i.vc);
 			break;
 		}

commit 72a154ce50be4a4154c3dc6b0936b57386ea4395

Fix clang check_header_includes=1
Fixes: 7bc0594e2147c17d30bc679d98ec6fcecb9830e3 ("`eclip`: change `changing_object_texture` into union of d1x, d2x")

common/main/bm.h
@@ -70,6 +70,7 @@ inline constexpr bool enable_bit_enum_boolnot<tmapinfo_flags>{true};
 
 }
 
+#ifdef DXX_BUILD_DESCENT
 namespace d1x {
 
 enum class object_bitmap_index : uint16_t
@@ -88,7 +89,6 @@ enum class object_bitmap_index : uint16_t
 
 }
 
-#ifdef DXX_BUILD_DESCENT
 namespace dsx {
 #if DXX_BUILD_DESCENT == 1
 constexpr std::integral_constant<unsigned, 800> MAX_TEXTURES{};