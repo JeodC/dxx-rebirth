JeodC/dxx-rebirth

commit 9772fe8c1d7dbb3b3a2dcb80641404fb032a524e

Rework countarray storage
- Choose the size_type based on the maximum value it may need to store,
  rather than defaulting to `unsigned`
- Add `constexpr` where practical
- Use `std::next()` to advance iterators
- Make `destroy` static, since it did not need access to member variables
- Make `destroy` run last-to-first instead of first-to-last
- Make `destroy` empty for trivially destructible types

common/include/countarray.h
@@ -10,41 +10,57 @@
 #include <cstddef>
 #include <memory>
 #include <stdexcept>
+#include <type_traits>
 #include "dxxsconf.h"
 #include <array>
 
-template <typename T = unsigned>
-class base_count_array_t
+namespace d_count_array {
+
+namespace detail {
+
+template <typename count_type>
+class count_storage
 {
 protected:
-	typedef T size_type;
-	void clear() { m_count = 0; }
-	size_type m_count = 0;
+	using size_type = count_type;
+	size_type m_count{};
 public:
-	size_type size() const { return m_count; }
-	bool empty() const { return !m_count; }
+	constexpr size_type size() const { return m_count; }
+	constexpr bool empty() const { return !m_count; }
 };
 
+template <std::size_t size>
+using base_count_value = count_storage<typename std::conditional_t<
+	(size <= UINT8_MAX), uint8_t,
+	typename std::conditional_t<(size <= UINT16_MAX), uint16_t, uint32_t>
+>>;
+
+}
+
+}
+
 template <typename T, std::size_t S>
-class count_array_t : public base_count_array_t<>
+class count_array_t : public d_count_array::detail::base_count_value<S>
 {
+	using base_count_value = d_count_array::detail::base_count_value<S>;
+	using base_count_value::m_count;
 public:
 	using array_type = std::array<T, S>;
-	typedef typename array_type::value_type value_type;
-	typedef typename array_type::iterator iterator;
-	typedef typename array_type::const_iterator const_iterator;
-	typedef typename array_type::const_reference const_reference;
-	static typename array_type::size_type max_size() { return S; }
+	using value_type = typename array_type::value_type;
+	using iterator = typename array_type::iterator;
+	using const_iterator = typename array_type::const_iterator;
+	using const_reference = typename array_type::const_reference;
+	static constexpr typename array_type::size_type max_size() { return {S}; }
+	auto data() const { return arrayref().data(); }
 	~count_array_t() { clear(); }
 	count_array_t &operator=(const count_array_t &rhs)
 	{
 		if (this != &rhs)
 		{
 			iterator lb = begin();
-			iterator le = end();
 			const_iterator rb = rhs.begin();
-			const_iterator re = rhs.end();
-			for (; lb != le && rb != re; ++lb, ++rb)
+			const const_iterator re{rhs.end()};
+			for (const auto le{end()}; lb != le && rb != re; ++lb, ++rb)
 				*lb = *rb;
 			shrink(lb);
 			for (; rb != re; ++rb)
@@ -75,10 +91,10 @@ public:
 		shrink(end() - 1);
 	}
 	iterator begin() { return arrayref().begin(); }
-	iterator end() { return arrayref().begin() + m_count; }
+	iterator end() { return std::next(begin(), m_count); }
 	iterator find(const T &t) { return std::find(begin(), end(), t); }
 	const_iterator find(const T &t) const { return std::find(begin(), end(), t); }
-	const_reference operator[](size_type i) const
+	const_reference operator[](const typename base_count_value::size_type i) const
 	{
 		if (i >= m_count)
 			throw std::out_of_range("not enough elements");
@@ -104,19 +120,26 @@ public:
 		std::replace(begin(), end(), o, n);
 	}
 	const_iterator begin() const { return arrayref().begin(); }
-	const_iterator end() const { return arrayref().begin() + m_count; }
+	const_iterator end() const { return std::next(begin(), m_count); }
 private:
-	void destroy(iterator b, iterator e)
+	static void destroy(const iterator b, iterator e)
 	{
-		for (; b != e; ++b)
-			b->~T();
+		if constexpr (std::is_trivially_destructible_v<T>)
+		{
+			(void)b;
+			(void)e;
+		}
+		else
+		{
+			for (; b != e;)
+				(-- e)->~T();
+		}
 	}
-	void shrink(iterator b)
+	void shrink(const iterator b)
 	{
 		destroy(b, end());
 		m_count = std::distance(begin(), b);
 	}
-private:
 	union U {
 		array_type m_data;
 		U() {}

commit 66d5bc729cb5197571a2780756b03d4c0825d1f0

Fix check_header_includes=1 for recent glibc
glibc reduced the set of included headers.  Explicitly include cstdint,
since it is used as of commit 9772fe8c1d7dbb3b3a2dcb80641404fb032a524e.

Fixes: 9772fe8c1d7dbb3b3a2dcb80641404fb032a524e ("Rework countarray storage")

common/include/countarray.h
@@ -8,6 +8,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <stdexcept>
 #include <type_traits>