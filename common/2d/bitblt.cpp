JeodC/dxx-rebirth

commit 9aac2a53b73258646fd28cc0c6e7fe19eeca5e6a

Option for fullscreen blits to fill popup menus in stereo viewports.
Popup menus are currently not squeezed for stereo viewports due to
lack of uniformity in x,y coordinates used by strings vs background
graphics. So propagate extra 'fill' parameter to show_fullscr() +
ogl_bitblt_cs() functions with defaults ignored by most callers.

common/2d/bitblt.cpp
@@ -489,14 +489,14 @@ inside:
 
 }
 
-void show_fullscr(grs_canvas &canvas, grs_bitmap &bm)
+void show_fullscr(grs_canvas &canvas, grs_bitmap &bm, bool fill)
 {
 	auto &scr = canvas.cv_bitmap;
 #if DXX_USE_OGL
 	if (bm.get_type() == bm_mode::linear && scr.get_type() == bm_mode::ogl &&
 		bm.bm_w <= grd_curscreen->get_screen_width() && bm.bm_h <= grd_curscreen->get_screen_height()) // only scale with OGL if bitmap is not bigger than screen size
 	{
-		ogl_ubitmapm_cs2x(canvas, 0, 0, -1, -1, bm, ogl_colors::white, F1_0);//use opengl to scale, faster and saves ram. -MPM
+		ogl_ubitmapm_cs(canvas, 0, 0, -1, -1, bm, ogl_colors::white, F1_0, fill);//use opengl to scale, faster and saves ram. -MPM
 		return;
 	}
 #endif

common/include/fwd-gr.h
@@ -226,7 +226,7 @@ void gr_uline(grs_canvas &canvas, fix x0,fix y0,fix x1,fix y1, color_palette_ind
 // Draw the bitmap into the current canvas at the specified location.
 void gr_bitmap(grs_canvas &, unsigned x,unsigned y,grs_bitmap &bm);
 void gr_ubitmap(grs_canvas &, grs_bitmap &bm);
-void show_fullscr(grs_canvas &, grs_bitmap &bm);
+void show_fullscr(grs_canvas &, grs_bitmap &bm, bool fill = true);
 
 // Find transparent area in bitmap
 void gr_bitblt_find_transparent_area(const grs_bitmap &bm, unsigned &minx, unsigned &miny, unsigned &maxx, unsigned &maxy);

common/include/ogl_init.h
@@ -97,7 +97,7 @@ private:
 void ogl_urect(grs_canvas &, int left, int top, int right, int bot, color_palette_index color);
 bool ogl_ubitmapm_cs(grs_canvas &, int x, int y,int dw, int dh, grs_bitmap &bm,int c, int scale);
 bool ogl_ubitmapm_cs(grs_canvas &, int x, int y,int dw, int dh, grs_bitmap &bm, const ogl_colors::array_type &c, int scale);
-bool ogl_ubitmapm_cs2x(grs_canvas &, int x, int y, int dw, int dh, grs_bitmap &bm, const ogl_colors::array_type &c, int scale);
+bool ogl_ubitmapm_cs(grs_canvas &, int x, int y, int dw, int dh, grs_bitmap &bm, const ogl_colors::array_type &c, int scale, bool fill);
 bool ogl_ubitblt_i(unsigned dw, unsigned dh, unsigned dx, unsigned dy, unsigned sw, unsigned sh, unsigned sx, unsigned sy, const grs_bitmap &src, grs_bitmap &dest, opengl_texture_filter texfilt);
 bool ogl_ubitblt(unsigned w, unsigned h, unsigned dx, unsigned dy, unsigned sx, unsigned sy, const grs_bitmap &src, grs_bitmap &dest);
 void ogl_upixelc(const grs_bitmap &, unsigned x, unsigned y, color_palette_index c);

similar/arch/ogl/ogl.cpp
@@ -2025,13 +2025,14 @@ bool ogl_ubitmapm_cs(grs_canvas &canvas, int x, int y,int dw, int dh, grs_bitmap
 	return 0;
 }
 
-bool ogl_ubitmapm_cs2x(grs_canvas &canvas, int x0, int y0, int dw, int dh, grs_bitmap &bm, const ogl_colors::array_type &color_array, int scale) // to scale bitmaps
+bool ogl_ubitmapm_cs(grs_canvas &canvas, int x0, int y0, int dw, int dh, grs_bitmap &bm, const ogl_colors::array_type &color_array, int scale, bool fill)
 {
 	// blit bitmap 2x for stereo viewport formats
 	if (VR_stereo) {
 		int x = x0, y = y0;
 		int w = (dw < 0) ? canvas.cv_bitmap.bm_w : (dw == 0) ? bm.bm_w : dw;
 		int h = (dh < 0) ? canvas.cv_bitmap.bm_h : (dh == 0) ? bm.bm_h : dh;
+		if (fill)
 		gr_stereo_viewport_resize(VR_stereo, w, h);
 		gr_stereo_viewport_offset(VR_stereo, x, y, -1);
 		ogl_ubitmapm_cs(canvas, x, y, w, h, bm, color_array, scale);

similar/main/newmenu.cpp
@@ -251,14 +251,14 @@ void nm_draw_background(grs_canvas &canvas, int x1, int y1, int x2, int y2)
 	gr_palette_load( gr_palette );
 	{
 		const auto &&tmp = gr_create_sub_canvas(canvas, x1, y1, w, h);
-		show_fullscr(*tmp, nm_background); // show so we load all necessary data for the sub-bitmap
+		show_fullscr(*tmp, nm_background, false); // show so we load all necessary data for the sub-bitmap
 	if (!init_sub && ((nm_background_sub->bm_w != w*((static_cast<float>(nm_background.bm_w))/SWIDTH)) || (nm_background_sub->bm_h != h*((static_cast<float>(nm_background.bm_h))/SHEIGHT))))
 	{
 		init_sub=1;
 	}
 	if (init_sub)
 		nm_background_sub = gr_create_sub_bitmap(nm_background,0,0,w*((static_cast<float>(nm_background.bm_w))/SWIDTH),h*((static_cast<float>(nm_background.bm_h))/SHEIGHT));
-	show_fullscr(*tmp, *nm_background_sub.get());
+	show_fullscr(*tmp, *nm_background_sub.get(), false);
 	}
 
 	gr_settransblend(canvas, 14, gr_blend::normal);

commit dc8ef466a7b362ea19d55cac3d437a92f134a86e

SDL-only: `show_fullscr`: cast away `fill`
Commit 9aac2a53b73258646fd28cc0c6e7fe19eeca5e6a added a parameter `fill`
to `show_fullscr` for the benefit of the OpenGL build.  SDL-only does
not use this parameter, so it triggers a `-Wunused` warning.  Cast away
the parameter to suppress the warning in the SDL-only build.  Changing
all callers not to pass this in SDL-only would be slightly more
efficient, but allowing the unused parameter is a fairly small cost,
only affects SDL-only users, and might be optimized out by use of the
Link Time Optimizer.

Fixes: 9aac2a53b73258646fd28cc0c6e7fe19eeca5e6a ("Option for fullscreen blits to fill popup menus in stereo viewports.")

common/2d/bitblt.cpp
@@ -473,6 +473,8 @@ void show_fullscr(grs_canvas &canvas, grs_bitmap &bm, bool fill)
 		ogl_ubitmapm_cs(canvas, 0, 0, opengl_bitmap_use_dst_canvas, opengl_bitmap_use_dst_canvas, bm, ogl_colors::white, fill);//use opengl to scale, faster and saves ram. -MPM
 		return;
 	}
+#else
+	static_cast<void>(fill);
 #endif
 	if (scr.get_type() != bm_mode::linear)
 	{