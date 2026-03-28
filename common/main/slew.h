JeodC/dxx-rebirth

commit 3ff057e7e76bde53e763dfd833537c7b565f86bf

`slew_init`: only define if it will be called

common/main/slew.h
@@ -25,16 +25,13 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #pragma once
 
-#ifdef __cplusplus
-#include "fwd-object.h"
 #include "dsx-ns.h"
 
 //from slew.c
 
 #if 1   //ndef RELEASE  //kill error on RELEASE build
-
-#ifdef DXX_BUILD_DESCENT
-void slew_init(vmobjptr_t obj);                // say this is slew obj
+#if !defined(RELEASE) || DXX_USE_EDITOR
+void slew_init(object &obj);                // say this is slew obj
 #endif
 int slew_stop(void);                            // Stops object
 void slew_reset_orient();                   // Resets orientation
@@ -46,11 +43,8 @@ int slew_frame(int dont_check_keys);        // Does slew frame
 
 #else
 
-#define slew_init(obj)
 #define slew_stop(obj)
 #define slew_reset_orient()
 #define slew_frame(dont_check_keys)
-
-#endif
 
 #endif

similar/main/slew.cpp
@@ -42,15 +42,17 @@ object *slew_obj=NULL;	//what object is slewing, or NULL if none
 
 // -------------------------------------------------------------------
 //say start slewing with this object
-void slew_init(const vmobjptr_t obj)
+#if !defined(RELEASE) || DXX_USE_EDITOR
+void slew_init(object &obj)
 {
-	slew_obj = obj;
+	slew_obj = &obj;
 
-	slew_obj->control_source = object::control_type::slew;
-	slew_obj->movement_source = object::movement_type::None;
+	obj.control_source = object::control_type::slew;
+	obj.movement_source = object::movement_type::None;
 
 	slew_stop();		//make sure not moving
 }
+#endif
 
 
 int slew_stop()

commit 3b2bb1afd30d62c7aac6d8bc4fdfc0318b299a12

`slew.h`: move various slew functions to `dsx`
Declare `object` when needed.

Fixes: 3ff057e7e76bde53e763dfd833537c7b565f86bf ("`slew_init`: only define if it will be called")

common/main/slew.h
@@ -30,14 +30,21 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 //from slew.c
 
 #if !defined(RELEASE) || DXX_USE_EDITOR
-void slew_init(object &obj);                // say this is slew obj
-int slew_stop();                            // Stops object
 #ifdef DXX_BUILD_DESCENT
 namespace dsx {
+
+struct object;
+
+void slew_init(object &obj);                // say this is slew obj
+int slew_stop();                            // Stops object
+
+#if !defined(RELEASE)
+
+void slew_reset_orient();                   // Resets orientation
+
+#endif
+
 int slew_frame();        // Does slew frame
 }
 #endif
-#endif
-#if !defined(RELEASE)
-void slew_reset_orient();                   // Resets orientation
 #endif

similar/main/slew.cpp
@@ -38,6 +38,8 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 //variables for slew system
 
+namespace dsx {
+
 object *slew_obj=NULL;	//what object is slewing, or NULL if none
 
 // -------------------------------------------------------------------
@@ -75,8 +77,6 @@ void slew_reset_orient()
    slew_obj->orient.uvec.z = slew_obj->orient.fvec.x = slew_obj->orient.fvec.y = 0;
 }
 #endif
-
-namespace dsx {
 
 namespace {
 