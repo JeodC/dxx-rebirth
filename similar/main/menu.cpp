3bd8d1e7b077e30698f9ae69d2236843cc6ec669
Jeod <47716344+JeodC@users.noreply.github.com>
2026-05-03 17:53:47 -0400

    Fix crash with menus

--- similar/main/menu.cpp
+++ similar/main/menu.cpp
@@ -1011,9 +1011,10 @@ window_event_result do_new_game_menu()
 				}
 				virtual window_event_result event_handler(const d_event &event) override
 				{
+					const auto alive = track();
 					const auto result = newmenu::event_handler(event);
-					if (result == window_event_result::close || result == window_event_result::deleted)
-						return result;
+					if (!*alive)
+						return window_event_result::deleted;
 					items.update_label();
 					return result;
 				}
